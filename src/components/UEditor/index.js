ad47d3ff3462fe72bcf6ddbc833c3c24b2704a5c
fan <www.5136.com@qq.com>
2019-11-14 20:00:25 +0800

    富文本

--- src/components/UEditor/index.js
+++ src/components/UEditor/index.js
@@ -1,62 +1,35 @@
-/**
- * Created by GuoMiaomiao on 2018/4/18.
- */
 import React from 'react';
-// import UE from '../ueditor/ueditor.all';
-const UE = window.UE;
-// let editor=null;
-class Ueditor extends React.Component {
-  static defaultProps = {
-    config: {},
-  }
 
-  constructor(props){
+export default class Ueditor extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      content:'',
-      editor:'',
+      id: this.props.id ? this.props.id : null,
+      ueditor: null,
     };
   }
-
-  componentDidMount(){
-    this.initEditor()
-  }
-
-  componentWillUnmount() {
-    // 组件卸载后，清除放入库的id
-    UE.delEditor(this.props.id);
-  }
-
-  initEditor() {
-    /*初始化编辑器*/
-    const { id, config } = this.props;
-    const ueEditor = UE.getEditor(this.props.id, config);
-    const self = this;
-
-    ueEditor.ready((ueditor) => {
-      if (!ueditor) {
+  componentDidMount() {
+    let UE = window.UE;
+    let { id } = this.state;
+    if (id) {
+      try {
+        /*加载之前先执行删除操作，否则如果存在页面切换，
+        再切回带编辑器页面重新加载时不刷新无法渲染出编辑器*/
         UE.delEditor(id);
-        self.initEditor();
-      }
-    });
-    let editor = ueEditor;
-    this.setState({editor});
+      } catch (e) {}
+      let ueditor = UE.getEditor(id, {
+        autoHeightEnabled: true,
+        autoFloatEnabled: true,
+      });
+      this.setState({ ueditor });
+    }
   }
-  getVal(){
-    /*获取编辑器内容函数*/
-    let {editor} = this.state;
-    let content = editor.getContent();
-    return content;
-  }
-  render(){
-    let { content,id } = this.props;
+  render() {
+    let { id } = this.state;
     return (
-      <div >
-        <textarea id={id}
-                  defaultValue={content}
-                  onChange={this.getVal}/>
+      <div>
+        <textarea id={id} />
       </div>
-    )
+    );
   }
 }
-export default Ueditor;
--- src/pages/content/addedit/index.js
+++ src/pages/content/addedit/index.js
@@ -1,35 +1,36 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import { Card, Button, Alert, Icon } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import Ueditor from '../../../components/UEditor';
+import styles from './index.less';
 
-export default class Addedit extends Component{
+export default class Addedit extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      initData: ''
-    }
+      //编辑、新增
+      editData: this.props.editType === 'add' ? '' : this.props.editType,
+    };
   }
 
   // 富文本编辑器 保存
-  saveEditor = () => {
-    console.log(this.refs.ueditor.getUEContent())
-  }
+  hanldeChage = value => {
+    var content = UE.getEditor('content').getContent();
 
+    // console.log('RcUeditor', value);
+    console.log(content);
+  };
 
-  render(){
-    return(
-      <PageHeaderWrapper>
+  render() {
+    return (
+      <PageHeaderWrapper title="">
         <Card>
-          <Ueditor id={'myeditor'}/>
-          {/* 使用UEditor 组件 */}
-          {/* <Ueditor value={formData.content} id="content" height="200" disabled={!this.props.canEdit}/>  */}
-          {/* <UEditor ref="ueditor" initData={this.state.initData} /> */}
-          <Button type="primary" onClick={this.saveEditor}>
+          <Ueditor id={'content'} className={styles.Ueditor} />
+          <Button type="primary" onClick={this.hanldeChage}>
             保存
           </Button>
         </Card>
       </PageHeaderWrapper>
-    )
+    );
   }
 }
